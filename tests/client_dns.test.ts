f010cb231ef5de2e08c54d660fdb94713a908e51
Alex Wellnitz <moin@wellnitz-alex.de>
2023-02-08 15:59:28 +0100

    Update DNS Client tests.

--- tests/client_dns.test.ts
+++ tests/client_dns.test.ts
@@ -1,25 +1,47 @@
 import { ClientDNS } from "../src"
 import { expect } from "chai";
 
+require('dotenv').config();
+
+const Token:string = (process.env['TOKEN'] as string);
+const Url:string = (process.env['URL'] as string);
+
 describe("hosting.de SDK DNS Client", () => {
     it("Constructs without throwing", () => {
-        let url = '',
-            token = '',
+        let url = Url,
+            token = Token,
             limit = 10;
         new ClientDNS(url, token, limit)
     })
-    it("Find Zones without throwing", async () => {
-        let url = '',
+
+    it("FindZones and get status 'success'", async () => {
+        let url = Url,
+            token = Token,
+            limit = 10;
+        const client = new ClientDNS(url, token, limit)
+        let result = await client.FindZones();
+        
+        expect(result.status).to.equal('success');
+    })
+
+    it("FindZones without Token and get status 'error'", async () => {
+        let url = Url,
             token = '',
             limit = 10;
         const client = new ClientDNS(url, token, limit)
         let result = await client.FindZones();
+
+        expect(result.status).to.equal('error');
+        expect(result.errors[0].code).to.equal(11002);
     })
-    it("Find ZoneConfigs without throwing", async () => {
-        let url = '',
-            token = '',
+
+    it("FindZoneConfigs and get status 'success'", async () => {
+        let url = Url,
+            token = Token,
             limit = 10;
         const client = new ClientDNS(url, token, limit)
         let result = await client.FindZoneConfigs();
+
+        expect(result.status).to.equal('success');
     })
 })
