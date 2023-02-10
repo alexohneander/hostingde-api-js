960ec5f60ff24f7dd156756e2cb0674f952f7c04
Alex Wellnitz <moin@wellnitz-alex.de>
2023-02-10 09:17:47 +0100

    New dns client methods

--- src/client_dns.ts
+++ src/client_dns.ts
@@ -6,6 +6,11 @@ import { Sort } from './types/sort';
 import { FindRecordsResult } from './types/dns/responses/find_records_result';
 import { FindZoneConfigsResult } from './types/dns/responses/find_zone_configs_result';
 import { FindZonesResult } from './types/dns/responses/find_zones_result';
+import { Template } from './types/dns/template';
+import { RecordTemplate } from './types/dns/record_template';
+import { CreateTemplateRequest } from './types/dns/requests/create_template_request';
+import { CreateTemplateResult } from './types/dns/responses/create_template_result';
+import { FindTemplatesResult } from './types/dns/responses/find_templates_result';
 
 export default class ClientDNS extends Client {
 	Location: string = `${this.Url}/dns/v1/json`;
@@ -50,6 +55,42 @@ export default class ClientDNS extends Client {
 		};
 
 		const res: FindRecordsResult = await PostRequest(req, `${this.Location}/recordsFind`);
+		return res;
+	}
+
+
+	public async FindTemplates(filter?: Filter, limit: number = 10, page: number = 1, sort?: Sort) {
+		const req: ClientRequest = {
+			authToken: this.Token,
+			limit: limit,
+			page: page,
+			filter: filter,
+			sort: sort,
+		};
+
+		const res: FindTemplatesResult = await PostRequest(req, `${this.Location}/templatesFind`);
+		return res;
+	}
+
+	public async CreateTemplate(dnsTemplate: Template, recordTemplates: RecordTemplate[]) {
+		const req: CreateTemplateRequest = {
+			authToken: this.Token,
+			dnsTemplate: dnsTemplate,
+			recordTemplates: recordTemplates,
+		};
+
+		const res: CreateTemplateResult = await PostRequest(req, `${this.Location}/templateCreate`);
+		return res;
+	}
+
+	public async DeleteTemplate(templateId:string, templateName?:string) {
+		const req = {
+			authToken: this.Token,
+			templateId: templateId,
+			templateName: templateName
+		}
+
+		const res = await PostRequest(req, `${this.Location}/templateDelete`);
 		return res;
 	}
 
