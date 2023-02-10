b51cd60d27c3543fe8a93d863d06e5cbb576b80d
Alex Wellnitz <a.wellnitz@apozin.de>
2023-02-10 12:18:17 +0100

    add new methods to dnsclient.

--- src/client_dns.ts
+++ src/client_dns.ts
@@ -1,6 +1,6 @@
 import Client from './client';
 import { PostRequest } from './helpers/http';
-import { ClientRequest } from './types/request';
+import { DNSFindRequest } from './types/dns/requests/dns_find_request';
 import { Filter } from './types/filter';
 import { Sort } from './types/sort';
 import { FindRecordsResult } from './types/dns/responses/find_records_result';
@@ -11,6 +11,11 @@ import { RecordTemplate } from './types/dns/record_template';
 import { CreateTemplateRequest } from './types/dns/requests/create_template_request';
 import { CreateTemplateResult } from './types/dns/responses/create_template_result';
 import { FindTemplatesResult } from './types/dns/responses/find_templates_result';
+import { ZoneConfig } from './types/dns/zone_config';
+import { DNSRecord } from './types/dns/record';
+import { DNSSecOptions } from './types/dns/dns_sec_options';
+import { CreateZoneRequest } from './types/dns/requests/create_zone_request';
+import { CreateZoneResult } from './types/dns/responses/create_zone_result';
 
 export default class ClientDNS extends Client {
 	Location: string = `${this.Url}/dns/v1/json`;
@@ -19,21 +24,9 @@ export default class ClientDNS extends Client {
 		super(url, token, limit);
 	}
 
-	public async FindZoneConfigs(filter?: Filter, limit: number = 50, page: number = 1, sort?: Sort) {
-		const req: ClientRequest = {
-			authToken: this.Token,
-			limit: limit,
-			page: page,
-			filter: filter,
-			sort: sort,
-		};
-
-		const res: FindZoneConfigsResult = await PostRequest(req, `${this.Location}/zoneConfigsFind`);
-		return res;
-	}
-
+	// ZONES
 	public async FindZones(filter?: Filter, limit: number = 50, page: number = 1, sort?: Sort) {
-		const req: ClientRequest = {
+		const req: DNSFindRequest = {
 			authToken: this.Token,
 			limit: limit,
 			page: page,
@@ -45,8 +38,63 @@ export default class ClientDNS extends Client {
 		return res;
 	}
 
+	public async CreateZone(
+		zoneConfig: ZoneConfig,
+		records: DNSRecord[],
+		nameserverSetId?: string,
+		useDefaultNameserverSet?: boolean,
+		dnsSecOptions?: DNSSecOptions,
+	) {
+		const req: CreateZoneRequest = {
+			authToken: this.Token,
+			zoneConfig: zoneConfig,
+			records: records,
+			nameserverSetId: nameserverSetId,
+			useDefaultNameserverSet: useDefaultNameserverSet,
+			dnsSecOptions: dnsSecOptions,
+		};
+
+		const res: CreateZoneResult = await PostRequest(req, `${this.Location}/zoneCreate`);
+		return res;
+	}
+
+	public async RecreateZone(
+		zoneConfig: ZoneConfig,
+		records: DNSRecord[],
+		nameserverSetId?: string,
+		useDefaultNameserverSet?: boolean,
+		dnsSecOptions?: DNSSecOptions,
+	) {
+		const req: CreateZoneRequest = {
+			authToken: this.Token,
+			zoneConfig: zoneConfig,
+			records: records,
+			nameserverSetId: nameserverSetId,
+			useDefaultNameserverSet: useDefaultNameserverSet,
+			dnsSecOptions: dnsSecOptions,
+		};
+
+		const res: CreateZoneResult = await PostRequest(req, `${this.Location}/zoneRecreate`);
+		return res;
+	}
+
+	// ZONECONFIGS
+	public async FindZoneConfigs(filter?: Filter, limit: number = 50, page: number = 1, sort?: Sort) {
+		const req: DNSFindRequest = {
+			authToken: this.Token,
+			limit: limit,
+			page: page,
+			filter: filter,
+			sort: sort,
+		};
+
+		const res: FindZoneConfigsResult = await PostRequest(req, `${this.Location}/zoneConfigsFind`);
+		return res;
+	}
+
+	// RECORDS
 	public async FindRecords(filter?: Filter, limit: number = 10, page: number = 1, sort?: Sort) {
-		const req: ClientRequest = {
+		const req: DNSFindRequest = {
 			authToken: this.Token,
 			limit: limit,
 			page: page,
@@ -58,9 +106,9 @@ export default class ClientDNS extends Client {
 		return res;
 	}
 
-
+	// TEMPLATES
 	public async FindTemplates(filter?: Filter, limit: number = 10, page: number = 1, sort?: Sort) {
-		const req: ClientRequest = {
+		const req: DNSFindRequest = {
 			authToken: this.Token,
 			limit: limit,
 			page: page,
@@ -83,12 +131,12 @@ export default class ClientDNS extends Client {
 		return res;
 	}
 
-	public async DeleteTemplate(templateId:string, templateName?:string) {
+	public async DeleteTemplate(templateId: string, templateName?: string) {
 		const req = {
 			authToken: this.Token,
 			templateId: templateId,
-			templateName: templateName
-		}
+			templateName: templateName,
+		};
 
 		const res = await PostRequest(req, `${this.Location}/templateDelete`);
 		return res;
